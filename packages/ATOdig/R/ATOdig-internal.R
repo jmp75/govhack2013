d4bb7a0b5cb09653ade8bd53226db892df57004c
U-NEXUS\per202 <per202@KERALA-BU.nexus.csiro.au>
2013-06-02 16:30:36 +1000

    Add the ATO.dig R package

---
+++ packages/ATOdig/R/ATOdig-internal.R
@@ -0,0 +1,40 @@
+#' find the categories in the table extracted from the ATO excel file
+#'
+#' find the categories in the table extracted from the ATO excel file
+#'
+#' @param str_mat the character matrix 
+#' @return nothing is returned by this function
+findCategories <- function(str_mat) {
+  d <- str_mat[,1]
+  d[which(d != "")]
+}
+
+#' find the categories in the table extracted from the ATO excel file
+#'
+#' find the categories in the table extracted from the ATO excel file
+#'
+#' @param d the character matrix 
+#' @export
+#' @return a list of data frames, with the series for ATO data.
+extractSeries <- function(d) {
+  # FIXME: a LOT of assumptions are made about the spreadsheet data in here...
+  catIndex <- which(d[,1] != "")
+  lblIndex <- which(d[,2] != "")
+  # Well, assumptions have to be made given the unstructured data:
+  lblsPerCat <- length(lblIndex) / length(catIndex)
+
+  getDataCategory <- function(i) {
+    rows <- catIndex[i]+1:lblsPerCat
+    units <- d[rows,3]
+    series <- d[rows,4:ncol(d)]
+    series[series=="n.a."] <- NA
+    series.df <- as.data.frame(t(matrix(as.numeric(series), nrow=length(rows))))
+    names(series.df) <- d[rows,2]
+    attr(series.df, 'units') <- units
+    series.df
+  }
+  
+  result <- lapply(1:length(catIndex), FUN=getDataCategory)
+  result
+}
+
---
+++ packages/ATOdig/R/ATOdig-package.R
@@ -0,0 +1,25 @@
+#' A package to access the ATO Excel based data from R 
+#' 
+#' \tabular{ll}{
+#' Package: \tab ATOdig\cr
+#' Type: \tab Package\cr
+#' Version: \tab 0.1-3\cr
+#' Date: \tab 2013-06-03\cr
+#' License: \tab LGPL 2.1\cr
+#' }
+#'
+#' 
+#'
+#'
+#' @imports rClr
+#' @imports stringr
+#' @name ATOdig-package
+#' @aliases ATO.dig
+#' @docType package
+#' @title Access the ATO Excel based data from R
+#' @author Jean-Michel Perraud \email{jean-michel.perraud_at_csiro.au}
+#' @keywords package rClr .NET Mono ATO
+NULL
+
+
+
